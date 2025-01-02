7a5c19f5de64a10287ebddef61601641b5b237a0
Alexander Haas <63969999+ahaas99@users.noreply.github.com>
2025-01-02 15:42:18 +0100

    Update evaluate_multi_head_corrupted_website.py

--- evaluate_multi_head_corrupted_website.py
+++ evaluate_multi_head_corrupted_website.py
@@ -4,7 +4,7 @@ Chair of Explainable Machine Learning
 Otto-Friedrich University of Bamberg
 
 @description:
-The script evaluates a model on a specified dataset of the MedMNIST+ collection.
+The script evaluates a model on all datasets of MedMNIST-C and saves it as .csv in the form needed for submission for the website
 """
 
 # Import packages
